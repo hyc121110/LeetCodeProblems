be2f5ef899857bd2d2f808d93e2563a6476ce0e8
hchan069 <spagabtti@gmail.com>
2019-05-09 10:29:04 -0700

    new problem

---
+++ DP/rob1.py
@@ -0,0 +1,34 @@
+def rob(nums):
+  # relation: rob(i) = max(rob(i-2) + cur_val, rob(i-1))
+
+  # method 1: recursive (top-down) (slow)
+  # def helper(nums, i):
+  #   if i < 0:
+  #     return 0
+  #   return max(helper(nums, i-2) + nums[i], helper(nums, i-1))
+
+  # if len(nums) == 1:
+  #   return nums[0]
+  # return helper(nums, len(nums)-1)
+
+  # method 2: recursive memoization (top-down)
+  # def helper(nums, i, dp):
+  #   if i < 0: return 0
+  #   if dp[i] >= 0: return dp[i]
+  #   result = max(helper(nums, i-2, dp) + nums[i], helper(nums, i-1, dp))
+  #   dp[i] = result
+  #   return result
+
+  # dp = [-1] * (len(nums)+1)
+  # return helper(nums, len(nums)-1, dp)
+
+  # method 3: iterative, 2 variables (bottom-up)
+  if len(nums) == 0: return 0
+  prev1 = prev2 = 0
+  for num in nums:
+    tmp = prev1
+    prev1 = max(prev2 + num, prev1)
+    prev2 = tmp
+  return prev1
+
+print(rob(nums=[1,2,3,4,5]))
