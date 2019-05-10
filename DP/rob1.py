13713de4d34670c1d8b10997170e6e0c72a4e696
hchan069 <spagabtti@gmail.com>
2019-05-09 23:12:11 -0700

    added description

--- DP/rob1.py
+++ DP/rob1.py
@@ -1,3 +1,9 @@
+'''
+You are a professional robber planning to rob houses along a street. Each house has a certain amount of money stashed, the only constraint stopping you from robbing each of them is that adjacent houses have security system connected and it will automatically contact the police if two adjacent houses were broken into on the same night.
+
+Given a list of non-negative integers representing the amount of money of each house, determine the maximum amount of money you can rob tonight without alerting the police.
+'''
+
 def rob(nums):
   # relation: rob(i) = max(rob(i-2) + cur_val, rob(i-1))
 
@@ -11,24 +17,24 @@ def rob(nums):
   #   return nums[0]
   # return helper(nums, len(nums)-1)
 
-  # method 2: recursive memoization (top-down)
-  # def helper(nums, i, dp):
-  #   if i < 0: return 0
-  #   if dp[i] >= 0: return dp[i]
-  #   result = max(helper(nums, i-2, dp) + nums[i], helper(nums, i-1, dp))
-  #   dp[i] = result
-  #   return result
+  # method 2: recursive memoization (top-down) (fastest)
+  def helper(nums, i, dp):
+    if i < 0: return 0
+    if dp[i] >= 0: return dp[i]
+    result = max(helper(nums, i-2, dp) + nums[i], helper(nums, i-1, dp))
+    dp[i] = result
+    return result
 
-  # dp = [-1] * (len(nums)+1)
-  # return helper(nums, len(nums)-1, dp)
+  dp = [-1] * (len(nums)+1)
+  return helper(nums, len(nums)-1, dp)
 
-  # method 3: iterative, 2 variables (bottom-up)
-  if len(nums) == 0: return 0
-  prev1 = prev2 = 0
-  for num in nums:
-    tmp = prev1
-    prev1 = max(prev2 + num, prev1)
-    prev2 = tmp
-  return prev1
+  # # method 3: iterative, 2 variables (bottom-up)
+  # if len(nums) == 0: return 0
+  # prev1 = prev2 = 0
+  # for num in nums:
+  #   tmp = prev1
+  #   prev1 = max(prev2 + num, prev1)
+  #   prev2 = tmp
+  # return prev1
 
 print(rob(nums=[1,2,3,4,5]))
