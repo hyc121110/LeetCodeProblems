937f3387ff22ac39b8c54c30b1d7f4cd0a9f2097
hchan069 <spagabtti@gmail.com>
2019-05-11 15:27:20 -0700

    added description

--- Trees/kthSmallest.py
+++ Trees/kthSmallest.py
@@ -1,3 +1,7 @@
+'''
+Given a binary search tree, write a function kthSmallest to find the kth smallest element in it.
+'''
+
 # Definition for a binary tree node.
 class TreeNode:
     def __init__(self, x):
