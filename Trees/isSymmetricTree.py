0bb2627a1d48d55bc3ee1d1928bda752065eaf0e
hchan069 <spagabtti@gmail.com>
2019-05-13 08:50:20 -0700

    added description; added 2nd solution

--- Trees/isSymmetricTree.py
+++ Trees/isSymmetricTree.py
@@ -1,3 +1,7 @@
+'''
+Given a binary tree, check whether it is a mirror of itself (ie, symmetric around its center).
+'''
+
 # Definition for a binary tree node.
 class TreeNode:
     def __init__(self, x):
@@ -7,26 +11,40 @@ class TreeNode:
 
 def isSymmetricTree(root):
   # recursive solution
-  def isMirror(p, q):
-    # 1) both subtrees are null -> symmetric
-    if not p and not q: return True
+  # def isMirror(p, q):
+  #   # 1) both subtrees are null -> symmetric
+  #   if not p and not q: return True
 
-    # 2) not balanced -> not symmetric
-    elif not p or not q: return False
+  #   # 2) not balanced -> not symmetric
+  #   elif not p or not q: return False
 
-    # 3) both subtress are not null
-    # a) both node are the same
-    # b) left.left = right.right
-    # c) left.right = right.left
-    # must satisfy all 3 conditions to return true
-    return (p.val == q.val) and isMirror(p.left, q.right) and isMirror(p.right, q.left)
-  # if not root: return True
-  return not root or isMirror(root.left, root.right)
+  #   # 3) both subtress are not null
+  #   # a) both node are the same
+  #   # b) left.left = right.right
+  #   # c) left.right = right.left
+  #   # must satisfy all 3 conditions to return true
+  #   return (p.val == q.val) and isMirror(p.left, q.right) and isMirror(p.right, q.left)
+  # # if not root: return True
+  # return not root or isMirror(root.left, root.right)
 
-  # iterative
-    
-    
-    
+  # iterative solution
+  if not root: return True
+  
+  # init stack with root's children
+  stack = [(root.left, root.right)]
+
+  while stack:
+    pair = stack.pop()
+    left, right = pair[0], pair[1]
+    # 1) both subtrees are null -> continue until stack empty
+    if not left and not right: continue
+    # 2) not balanced / value not the same -> not symmetric
+    if not left and right or not right and left or left.val != right.val: return False
+    # 3) both subtrees are not null -> append for check
+    stack.append((left.left, right.right))
+    stack.append((left.right, right.left))
+  return True
+
 
 root = TreeNode(1)
 root.left = TreeNode(2)
