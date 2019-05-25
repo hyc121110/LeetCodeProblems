3dc650684c8b28f457621b5e65e4c5288508451f
hchan069 <spagabtti@gmail.com>
2019-05-25 09:45:08 -0700

    new problem

---
+++ Array/canPartition.py
@@ -0,0 +1,17 @@
+def canParititon(nums):
+  # continue if sum of nums is even
+  if sum(nums) & 1 == 0:
+    # shift one bit to the right to find the sum of partition array
+    target = sum(nums) >> 1
+    # initialize set to 0
+    cur = {0}
+    for i in nums:
+      # perform union operation (|) with rhs to add 
+      cur |= {i + x for x in cur}
+      if target in cur:
+        return True
+  return False
+
+print(canParititon(nums=[1,5,11,5]))
+print(canParititon(nums=[1,2,3,4,5,6,7]))
+print(canParititon(nums=[1,1]))
