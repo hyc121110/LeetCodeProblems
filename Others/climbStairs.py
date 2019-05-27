18e9c5bb9db94cc0457648d54917d40e80507ab7
hchan069 <spagabtti@gmail.com>
2019-05-27 13:38:47 -0700

    new problem

---
+++ Others/climbStairs.py
@@ -0,0 +1,19 @@
+'''
+You are climbing a stair case. It takes n steps to reach to the top.
+
+Each time you can either climb 1 or 2 steps. In how many distinct ways can you climb to the top?
+
+Note: Given n will be a positive integer.
+'''
+
+'''
+Variable a tells you the number of ways to reach the current step, and b tells you the number of ways to reach the next step. So for the situation one step further up, the old b becomes the new a, and the new b is the old a+b, since that new step can be reached by climbing 1 step from what b represented or 2 steps from what a represented.
+'''
+
+def climbStairs(n):
+  a = b = 1
+  for _ in range(n):
+    a, b = b, a + b
+  return a
+
+print(climbStairs(4))
