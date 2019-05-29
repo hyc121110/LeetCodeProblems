fe63b9bfed1ad3db865a5bac5e55e906c6ae5ae1
hchan069 <spagabtti@gmail.com>
2019-05-28 21:08:43 -0700

    added description

--- Array/maxProfit.py
+++ Array/maxProfit.py
@@ -1,3 +1,11 @@
+'''
+Say you have an array for which the ith element is the price of a given stock on day i.
+
+If you were only permitted to complete at most one transaction (i.e., buy one and sell one share of the stock), design an algorithm to find the maximum profit.
+
+Note that you cannot sell a stock before you buy one.
+'''
+
 def maxProfit(prices):
   # max_cur tracks the difference of max[i] and future max[i]s
   max_cur = max_so_far = 0
