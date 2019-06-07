59f6b3bdf67628c59e7d8480b62210adf4f28c93
hchan069 <spagabtti@gmail.com>
2019-06-06 17:08:17 -0700

    added description

--- Sum/maxSumAfterPartitioning.py
+++ Sum/maxSumAfterPartitioning.py
@@ -1,3 +1,9 @@
+'''
+Given an integer array A, you partition the array into (contiguous) subarrays of length at most K.  After partitioning, each subarray has their values changed to become the maximum value of that subarray.
+
+Return the largest sum of the given array after partitioning.
+'''
+
 def maxSumAfterPartitioning(A, k):
   pass
 
