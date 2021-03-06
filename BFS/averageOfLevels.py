50d7bcad41d2a5de033dc4f8df53b8f1db51cb72
Evan Chan <spagabtti@gmail.com>
2021-03-06 11:31:09 +0800

    new problem

---
+++ BFS/averageOfLevels.py
@@ -0,0 +1,37 @@
+"""
+Given a non-empty binary tree, return the average value of the nodes on each level in the form of an array.
+"""
+
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def averageOfLevels(self, root: TreeNode) -> List[float]:
+        if not root:
+            return []
+        
+        ret = []
+        this_level = [root]
+        
+        while this_level:
+            # get the number of nodes in the current level
+            n = len(this_level)
+            _sum = 0 # initialize the sum of the current level
+            
+            for i in range(n): # current level iteration
+                node = this_level.pop(0)
+                _sum += node.val
+                
+                if node.left:
+                    this_level.append(node.left)
+                if node.right:
+                    this_level.append(node.right)
+            ret.append(_sum / n)
+            
+                
+        return ret
+        
+        
